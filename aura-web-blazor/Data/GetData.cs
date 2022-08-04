365e16466c9e4b6ff4074c20151e806fa13c4093
bledibledi <bledibllaca12@gmail.com>
2022-08-04 21:51:49 +0200

    Add project files.

---
+++ aura-web-blazor/Data/GetData.cs
@@ -0,0 +1,33 @@
+﻿using aura_web_blazor.Data.Models;
+using System.Text.Json;
+
+namespace aura_web_blazor.Data
+{
+    public class GetData
+    {
+        public Task<List<DetaletEFaturimit>> GetDetaletAsync()
+        {
+            
+            return Task.FromResult(JsonSerializer.Deserialize<List<DetaletEFaturimit>>(ReadJsonFromFile()));
+        }
+
+        public string ReadJsonFromFile()
+        {
+            string json = string.Empty;
+            try
+            {
+                using (StreamReader r = new StreamReader("Data/data.json"))
+                {
+                    json = r.ReadToEnd();
+                }
+            }
+            catch (Exception e)
+            {
+                //throw e;
+            }
+            json = json.Replace("0.0000", "0");
+
+            return json.Replace("<EOF>", "");
+        }
+    }
+}
---
+++ aura-web-blazor/Data/Models/DetaletEFaturimit.cs
@@ -0,0 +1,24 @@
+﻿namespace aura_web_blazor.Data.Models
+{
+    public class DetaletEFaturimit
+    {
+        public string artikulli { get; set; }
+        public string sasia { get; set; }
+        public string nj2 { get; set; }
+        public string qmimi { get; set; }
+        public string password { get; set; }
+        public string kam { get; set; }
+        public string ora { get; set; }
+        public string data { get; set; }
+        public string tav { get; set; }
+        public string vlera { get; set; }
+        public string nrPorosise { get; set; }
+        public string eshteMbyllur { get; set; }
+
+
+        override public string ToString()
+        {
+            return $"\n \"artikulli\": \"{artikulli}\",\n  \"sasia\": \"{sasia}\",\n \"qmimi\": \"{qmimi}\",\n \"vlera\": \"{vlera}\",\n \"nj2\": \"{nj2}\",\n  \"password\": \"{password}\",\n  \"kam\": \"{kam}\",\n  \"ora\": \"{ora}\",\n  \"data\": \"{data}\",\n  \"tav\": \"{tav}\",\n  \"nrPorosise\": \"{nrPorosise}\",\n  \"eshteMbyllur\": \"{eshteMbyllur}\",\n";
+        }
+    }
+}
