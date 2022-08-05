366da94dbaa7502e093ee9f071dcb5effdc8729d
bledibledi <bledibllaca12@gmail.com>
2022-08-05 02:53:56 +0200

    main totali expand tab finished

---
+++ aura-web-blazor/Data/Categories.cs
@@ -0,0 +1,15 @@
+﻿namespace aura_web_blazor.Data
+{
+    public class Categories
+    {
+        public static Dictionary<string, string> Category = new Dictionary<string, string>();
+        static Categories(){
+            Category.Add("Banaku", "B");
+            Category.Add("Kuzhina","K");
+            //Category.Add("Pizza", "P");
+            //Category.Add("Embelsira", "E");
+            //Category.Add("Sallata", "S");
+        }
+    }
+    
+}
