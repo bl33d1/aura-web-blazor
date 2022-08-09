672570ec6141ffb2be9bc484c5842c8ffe2bb8ca
bledibledi <bledibllaca12@gmail.com>
2022-08-10 00:23:48 +0200

    main page gati/ tavolinat page in progress/renamed all components

--- aura-web-blazor/Data/Categories.cs
+++ aura-web-blazor/Data/Categories.cs
@@ -3,13 +3,13 @@
     public class Categories
     {
         public static Dictionary<string, string> Category = new Dictionary<string, string>();
-        static Categories(){
-
+        static Categories()
+        {
             Category.Add("Banaku", "B");
             Category.Add("Kuzhina","K");
-        //    Category.Add("Pizza", "P");
-        //    Category.Add("Embelsira", "E");
-        //    Category.Add("Sallata", "S");
+            //Category.Add("Pizza", "P");
+            //Category.Add("Embelsira", "E");
+            //Category.Add("Sallata", "S");
         }
     }
     
