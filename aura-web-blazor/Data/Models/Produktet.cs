fefd729763294173a0ce1eabc5cc15826d7208fe
bledibledi <bledibllaca12@gmail.com>
2022-08-13 04:28:54 +0200

    tabs added te produktet/ nav bar icons and links added/ detajet e tavolines added/ pagination added for mainOrders/minor ui changes

---
+++ aura-web-blazor/Data/Models/Produktet.cs
@@ -0,0 +1,11 @@
+﻿namespace aura_web_blazor.Data.Models
+{
+    public class Produktet
+    {
+        public string Produkti { get; set; }
+        public int Sasia { get; set; }
+        public decimal Qmimi { get; set; }
+        public decimal Vlera { get; set; }
+        public string Nj2 { get; set; }
+    }
+}
